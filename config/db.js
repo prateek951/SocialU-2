d1806615aed522c5b3f72bfcf810f66318648e45
prateek951 <prince.cenation@gmail.com>
2019-07-09 08:04:13 +0530

    added the database connection

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const DATABASE = config.get('MONGO_URI');
+
+const CONNECT_DATABASE = async () => {
+  try {
+    await mongoose.connect(DATABASE, {
+      useNewUrlParser: true
+    });
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error(error.message);
+    // Exit the process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = CONNECT_DATABASE;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const CONNECT_DATABASE = require('./config/db');
 const app = express();
+
+// Connect to the database
+CONNECT_DATABASE();
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port,() => { 
+app.listen(port, () => {
   console.log(`Magin on port : ${port}`);
-})
+});
