ebb3a876b7fbd64671fef33c295bd8748e0fdfe5
prateek951 <prince.cenation@gmail.com>
2019-07-13 22:09:37 +0530

    feat: added the post schema mongoose model

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+/**
+ * @model Post
+ * Every post is associated to some user
+ * Each user can have several posts
+ */
+
+const { Schema, model } = require('mongoose');
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = model('post', PostSchema);
