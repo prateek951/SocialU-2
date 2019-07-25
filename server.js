9369aa60486175a0834435f889f56e86f4a0cf38
prateek951 <prince.cenation@gmail.com>
2019-07-25 06:04:44 +0530

    serving static assets in production mode

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const CONNECT_DATABASE = require('./config/db');
+const path = require('path');
 const app = express();
 
 // Connect to the database
 CONNECT_DATABASE();
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
+// app.get('/', (req, res) => {
+//   res.send('API Running');
+// });
 
 // body parser middleware
 app.use(express.json({ extended: false }));
@@ -18,7 +19,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set the static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Magin on port : ${port}`);
+  console.log(`Magic on port : ${port}`);
 });
